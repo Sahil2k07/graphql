370722e35fe7c8a85040e8ed1dce7a18be9685ad
Sahil2k07 <sahilsharma2k07@gmail.com>
2025-11-08 15:20:40 +0530

    Implement AWS Lambda integration with GraphQL server

---
+++ cmd/serverless/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/Sahil2k07/graphql/internal/configs"
+	"github.com/Sahil2k07/graphql/internal/database"
+	"github.com/Sahil2k07/graphql/internal/graphql/directives"
+	"github.com/Sahil2k07/graphql/internal/graphql/generated"
+	"github.com/Sahil2k07/graphql/internal/middlewares"
+	"github.com/Sahil2k07/graphql/internal/web"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	echoadapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var echoLambda *echoadapter.EchoLambda
+
+func lambdaHandler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return echoLambda.Proxy(req)
+}
+
+func init() {
+	configs := configs.LoadConfig()
+	database.Connect()
+
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     configs.Server.Origins,
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.PATCH},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowCredentials: true,
+	}))
+
+	e.Use(middlewares.JWTContext())
+
+	gqlHandler := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{
+				Resolvers: web.Resolvers(),
+				Directives: generated.DirectiveRoot{
+					Public: directives.AuthDirective(),
+				},
+			},
+		),
+	)
+
+	e.POST("/graphql", echo.WrapHandler(gqlHandler))
+	e.GET("/", echo.WrapHandler(playground.Handler("GraphQL playground", "/graphql")))
+
+	echoLambda = echoadapter.New(e)
+}
+
+func main() {
+	lambda.Start(lambdaHandler)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.24.2
 require (
 	github.com/99designs/gqlgen v0.17.81
 	github.com/BurntSushi/toml v1.5.0
+	github.com/aws/aws-lambda-go v1.50.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
 	github.com/charmbracelet/log v0.4.2
 	github.com/labstack/echo/v4 v4.13.4
 	github.com/lestrrat-go/jwx/v2 v2.1.6
@@ -52,7 +54,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/exp v0.0.0-20240112132812-db7319d0e0e3 // indirect
 	golang.org/x/net v0.46.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
